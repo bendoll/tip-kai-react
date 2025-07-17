4c65b5499c50ad2b630c225bbfe948fe4c29c4c1
Benjamin Doll <benjamin@benji.to>
2025-07-17 04:56:20 -0400

    Build@20250717T0450

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tip-kai-react/sw.js', { scope: '/tip-kai-react/' })})}
